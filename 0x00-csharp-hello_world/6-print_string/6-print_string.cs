c6395186ffaa6171718eb503b8d848dda1075a3c
Katorea <katorea132@gmail.com>
2020-11-30 17:52:51 -0500

    Checking all the other checks

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,13 +1,8 @@
-﻿using System;
-
-namespace _6_print_string
+﻿class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string str = "Holberton School";
-            Console.WriteLine($"{str}{str}{str}\n{str.Substring(0,9)}");
-        }
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        Console.WriteLine($"{str}{str}{str}\n{str.Substring(0,9)}");
     }
 }
