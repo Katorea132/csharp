c65a90d10f9eacba50d750155c9b092aa6cf87dc
Katorea <katorea132@gmail.com>
2020-11-30 17:43:57 -0500

    Adding up to point 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _6_print_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Holberton School";
+            Console.WriteLine($"{str}{str}{str}\n{str.Substring(0,9)}");
+        }
+    }
+}
