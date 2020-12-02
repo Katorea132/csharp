8fdc78a3169fe7ab497ff2146a16c316f6e56cbd
Katorea <katorea132@gmail.com>
2020-12-01 21:42:37 -0500

    Added 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i = 1; i < 101; i++)
+            {
+                if (i == 100)
+                    Console.WriteLine("Buzz");
+                else
+                {
+                    if (i % 5 == 0 && i % 3 == 0)
+                        Console.Write("FizzBuzz ");
+                    else if (i % 5 == 0)
+                        Console.Write("Buzz ");
+                    else if (i % 3 == 0)
+                        Console.Write("Fizz ");
+                    else
+                        Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
