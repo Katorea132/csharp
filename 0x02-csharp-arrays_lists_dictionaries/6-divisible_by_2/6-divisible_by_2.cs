f7d9c40fb7a39a1fe4ff307b5cfa1576b14d6585
Katorea <katorea132@gmail.com>
2020-12-02 16:54:50 -0500

    Adding 6 and 7

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> lili = new List<bool>();
+
+        foreach (int number in myList)
+        {
+            if (number % 2 == 0)
+                lili.Add(true);
+            else
+                lili.Add(false);
+        }
+        return lili;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index >= 0 && index < myList.Count)
+            myList.Remove(index + 1);
+        else
+            Console.WriteLine("Index is out of range");
+        return myList;
+    }
+}
