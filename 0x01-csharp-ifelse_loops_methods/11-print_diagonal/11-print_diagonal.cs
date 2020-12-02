09cf7d2b4fd8b82cba689b8d4ded2d1bcca96c09
Katorea <katorea132@gmail.com>
2020-12-01 21:31:36 -0500

    Adding 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int i, j;
+
+        for (i = 0; i < length; i++)
+        {
+            for (j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine();
+    }
+}
