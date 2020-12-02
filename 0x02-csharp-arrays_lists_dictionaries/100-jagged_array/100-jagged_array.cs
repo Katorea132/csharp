48972d4d99d83bad39b1f2aee001592886577400
Katorea <katorea132@gmail.com>
2020-12-02 18:04:11 -0500

    Adding last 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggy =
+            {
+                new int[] { 0, 1, 2, 3 },
+                new int[] { 0, 1, 2, 3, 4, 5, 6 },
+                new int[] { 0, 1 }
+            };
+            for (int i = 0; i < jaggy.Length; i++)
+                for (int j = 0; j < jaggy[i].Length; j++)
+                {
+                    if (j != jaggy[i].Length - 1)
+                        Console.Write($"{jaggy[i][j]} ");
+                    else
+                        Console.WriteLine(jaggy[i][j]);
+                }
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arry = new int[5, 5];
+
+            arry[2, 2] = 1;
+            for(int i = 0; i < arry.GetLength(0); i++)
+                for(int j = 0; j < arry.GetLength(1); j++)
+                {
+                    if (j != arry.GetLength(1) - 1)
+                        Console.Write($"{arry[i, j]} ");
+                    else
+                        Console.WriteLine(arry[j,i]);
+                }
+        }
+    }
+}
