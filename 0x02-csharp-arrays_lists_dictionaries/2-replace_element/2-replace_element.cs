c8c3ccca3694c6310b3c59aa870fdc5a315129c2
Katorea <katorea132@gmail.com>
2020-12-02 16:38:41 -0500

    Fixed missing curly brackets

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -5,8 +5,10 @@ class Array
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
         if ((index < array.Length) && (index >= 0))
+        {
             array[index] = n;
             return array;
+        }
         Console.WriteLine("Index out of range");
         return array;
     }
