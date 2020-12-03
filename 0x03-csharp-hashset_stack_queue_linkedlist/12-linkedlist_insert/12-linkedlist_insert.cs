7a5bab4cd1e1ffa196783c7cf993da3291f33b52
Katorea <katorea132@gmail.com>
2020-12-03 16:28:54 -0500

    Added 11 and 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        for (LinkedListNode<int> nodi = myLList.Last; nodi != null; nodi = nodi.Previous)
+        {
+            if (n > nodi.Value)
+                return myLList.AddAfter(nodi, n);
+            else if(nodi.Previous == null)
+                return myLList.AddBefore(nodi, n);
+        }
+        return null;
+    }
+}
