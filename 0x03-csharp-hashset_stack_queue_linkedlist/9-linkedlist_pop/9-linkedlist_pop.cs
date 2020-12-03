0557086cc36dbc539a5af649c84ab15e4250e4ba
Katorea <katorea132@gmail.com>
2020-12-03 16:11:35 -0500

    Adding from 6 to 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count != 0)
+        {
+            int number = myLList.First.Value;
+            myLList.RemoveFirst();
+            return number;
+        }
+        return 0;
+    }
+}
