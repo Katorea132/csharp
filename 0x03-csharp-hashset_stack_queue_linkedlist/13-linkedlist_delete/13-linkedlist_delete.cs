115da1cfd5a5f3515520cd65a5137eb38813b94a
Katorea <katorea132@gmail.com>
2020-12-03 16:35:10 -0500

    Added 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> nodi = myLList.First;
+
+        for (int i = 0; nodi != null; nodi = nodi.Next, i++)
+            if (i == index)
+                myLList.Remove(nodi);
+    }
+}
