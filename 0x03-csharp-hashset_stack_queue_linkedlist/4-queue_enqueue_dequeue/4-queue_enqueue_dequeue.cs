6209f3d29a4a8b3aee170be53557ec6bfabd43a5
Katorea <katorea132@gmail.com>
2020-12-03 15:38:48 -0500

    Adding 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        if (aQueue.Count != 0)
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        else
+            Console.WriteLine("Queue is empty");
+        aQueue.Enqueue(newItem);
+        
+        bool doesItTho = aQueue.Contains(search);
+
+        Console.WriteLine($"Queue contains {search}: {doesItTho}");
+        if (doesItTho)
+        {
+            string holder = "";
+            while (holder != search)
+                holder = aQueue.Dequeue();
+        }
+        return aQueue;
+    }
+}
