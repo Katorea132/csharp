02bc8fddd72487732eb394aca02294fe9fb7fc51
Katorea <katorea132@gmail.com>
2020-12-14 14:56:19 -0500

    Adding all 4 points, including readme

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int ans = 0;
+        try
+        {
+            ans = a / b;
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {ans}");
+        }
+    }
+}
