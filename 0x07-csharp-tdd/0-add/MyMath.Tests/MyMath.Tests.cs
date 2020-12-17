276980be71040d429cbc31347e38d692854ccb6a
Katorea <katorea132@gmail.com>
2020-12-17 16:28:25 -0500

    Adding the first task and README file

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(1, 0)]
+        [TestCase(0, 0)]
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        public void AddNumbers_ReturnsCorrectly(int a, int b)
+        {
+            Assert.AreEqual(MyMath.Operations.Add(a, b), a + b);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class for operations, currently only with
+    /// additions.
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Sums a and b.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>The sum of a and b.</returns>
+        public static int Add(int a, int b) => a + b;
+    }
+}
